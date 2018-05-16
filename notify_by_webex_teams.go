87f925a49c1259cf8007a2f95c044e564a7b2274
Gogs <gogs@fake.local>
2018-05-16 22:38:37 +0200

    version flag added

--- notify_by_webex_teams.go
+++ notify_by_webex_teams.go
@@ -1,34 +1,49 @@
+// 	file: notify_by_webex_teams.go
+// 	Version 0.1 (16.05.2018)
+//
+// CLI command for sending messages to Cisco Webex Teams rooms
+// by Herwig Grimm (herwig.grimm at aon.at)
 package main
 
 import (
 	"flag"
+	"fmt"
 	"log"
+	"os"
+	"path"
 
 	"github.com/vallard/spark"
 )
 
 var (
-	apiToken string
-	teamName string
-	roomName string
-	msgText  string
+	apiToken    string
+	teamName    string
+	roomName    string
+	msgText     string
+	showVersion bool
 )
 
 func init() {
 	flag.StringVar(&apiToken, "T", "", "Webex Teams API token")
 	flag.StringVar(&teamName, "t", "KMP-Developer-Team", "team name")
-	flag.StringVar(&roomName, "r", "Incident x", "new room name")
-	flag.StringVar(&msgText, "m", "test message", "messages text")
+	flag.StringVar(&roomName, "r", "Room1", "new room name")
+	flag.StringVar(&msgText, "m", "test message (markdown format)", "messages text")
+	flag.BoolVar(&showVersion, "V", false, "print program version")
 }
 
 const (
 	roomsURL = "https://api.ciscospark.com/v1/rooms"
+	version  = "0.1"
 )
 
 func main() {
 
-
 	flag.Parse()
+
+	if showVersion {
+		fmt.Printf("%s version: %s\n", path.Base(os.Args[0]), version)
+		os.Exit(0)
+	}
 
 	if len(apiToken) == 0 {
 		log.Fatalf("missing Webex Teams API token (flag -T)\n")
@@ -47,13 +62,11 @@ func main() {
 	newRoom, err := s.GetRoomWithName(roomName)
 
 	if err != nil {
-			newRoom, err = s.CreateRoom(roomName, room.TeamId)
-			if err != nil {
-				log.Fatalf("cannot create room >>%s<<: %s", roomName, err)
-			}
+		newRoom, err = s.CreateRoom(roomName, room.TeamId)
+		if err != nil {
+			log.Fatalf("cannot create room >>%s<<: %s", roomName, err)
+		}
 	}
-
-
 
 	// Create the message we want to send
 	m := spark.Message{
@@ -67,5 +80,5 @@ func main() {
 	if err != nil {
 		log.Fatalf("cannot create message >>%s<<: %s", roomName, err)
 	}
-	
+
 }
