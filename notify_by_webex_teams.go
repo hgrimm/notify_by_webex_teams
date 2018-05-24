e01abdbf65d5cbb954f024960d9472a83c269166
Herwig Grimm <herwig.grimm@aon.at>
2018-05-24 11:47:35 +0200

    More content in README.md and leading comments in main program file

--- notify_by_webex_teams.go
+++ notify_by_webex_teams.go
@@ -1,6 +1,10 @@
-// Send messages and files to Webex Teams. If <room name> is not found a new room is created.
-// Supports on file upload per request. 
-// 
+// notify_by_webex_teams.go
+//
+// CLI command for sending messages to Cisco Webex Teams rooms.
+// Send messages and files to Webex Teams. If *room name* is not found a new room is created.
+// Supports on file upload per request.
+// by Herwig Grimm (herwig.grimm at aon.at)
+//
 // required args:
 //			-T <Webex Teams API token>
 //			-t <team name>
@@ -16,7 +20,7 @@
 //
 // doc links:
 //			https://developer.webex.com/getting-started.html
-// 			
+//
 package main
 
 import (
